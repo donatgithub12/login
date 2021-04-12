bd07aa7f8503f946b66a437bda5dd87a90575e65
alen130 <alenvarghese90@gmail.com>
2021-04-12 16:17:03 +0530

    12/04/2021 password reset completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView,LogoutView,PasswordResetView,PasswordResetConfirmView,PasswordResetDoneView,PasswordResetCompleteView
 from django.contrib.auth import views as auth_views
+
 urlpatterns=[
+     #path('', include('django.contrib.auth.urls')),
     path('',views.indexView,name="home"),
     path('dashboard/',views.dashboardView,name="dashboard"),
     path('login/',LoginView.as_view(),name="login_url"),
     path('register/',views.registerView,name="register_url"),
     path('logout/',LogoutView.as_view(next_page='dashboard'),name="logout"),
+   
 
-
-    path('password_reset',auth_views.PasswordResetView.as_view(),name='password_reset'),
-   # path('password_reset/email',auth_views.PasswordResetEmailView.as_view(),name='password_reset_email'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/token/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset',PasswordResetView.as_view(),name="password_reset"),
+    path('password_reset/done',PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('password_reset_confirm/<uidb64>/<token>',PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('password_reset/complete/',PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 
 ]
--- login/settings.py
+++ login/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'password_reset',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/accounts/dashboard/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_HOST = 'localhost'
+#EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'roadmaker49@gmail.com'
+EMAIL_HOST_PASSWORD = 'roadmaker@123'
