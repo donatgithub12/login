b61413eb4ce28c8cd609c7fabd39bf8eb803c1f0
alen130 <alenvarghese90@gmail.com>
2021-04-13 09:05:27 +0530

    register redirected to login 13/04/21

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
@@ -15,6 +15,8 @@ def registerView(request):
         if form.is_valid():
             form.save()
             return redirect('login_url')
+       
     else:
         form = SignUpForm()    
     return render(request,'registration/register.html',{'form':form})     
+  
